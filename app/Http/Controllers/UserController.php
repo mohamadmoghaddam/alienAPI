74c72f17f06ab990c50503b165a9d51ed16d9edc
mohamad <mohamadreza.ghasemi1383@gmail.com>
2023-09-16 14:19:49 +0430

    create index function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,7 +12,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        return response("yes");
+        $users = User::all()->toJson();
+        return response($users)
+        ->header('Content-Type', 'application/json');
     }
 
     /**
