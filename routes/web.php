bb0755008a8c1d241b3b5ca457aab1097ab40a74
mohamad <mohamadreza.ghasemi1383@gmail.com>
2023-09-19 17:13:59 +0330

    create register route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/login', function() {
 })->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
