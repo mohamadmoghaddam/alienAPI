b1362a3d42e2110bf869c14b67eb1254d2694b66
mohamad <mohamadreza.ghasemi1383@gmail.com>
2023-09-20 12:36:44 +0330

    update Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Chat;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)
+        ->has(Chat::factory(3)->group())
+        ->has(Chat::factory(3)->private())
+        ->create();
     }
 }
